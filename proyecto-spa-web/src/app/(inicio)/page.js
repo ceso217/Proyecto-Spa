75c3861cee791035fa612fccf86f70e6debd674d
LeonardoBrabo <leonardo.david.brabo@gmail.com>
2024-09-02 09:50:07 -0300

    agregado apartado de services, mapa en home y  texto de mensaje de empresa

--- proyecto-spa-web/src/app/(inicio)/page.js
+++ proyecto-spa-web/src/app/(inicio)/page.js
@@ -31,26 +31,63 @@ export default function Home() {
       </div>
     </div>
       <div className="Somos">
-        <div className = "item">
-          <h2>Vive una experiencia inolvidable</h2>
-        </div>
-        <div className = "item">
+        <div className ="item">
             <Image
               src= "/fotopiedras.svg"
               alt = "fotos de persona recibiendo masaje"
-              width = {300}
-              height={200}
+              width = {400}
+              height={600}
             />
-          </div>
-          <div className = "item">
+        </div>
+        <div className = "item">
+          <h2>Quienes Somos</h2>
+          <p>Buscamos atraer la atención de nuestros clientes a través de experiencias inspiradas en la seducción de los sentidos. Adaptamos las propuestas con el objetivo de que logre desconectarse completamente de la rutina y disfrute de un momento de bienestar, en total armonía con la naturaleza.</p>
+        </div>
+        <div className = "item i2">
             <Image
               src= "/fotomasaje.svg"
               alt = "fotod de persona recibiendo masaje"
-              width = {300}
-              height={200}
+              width = {400}
+              height={600}
             />
-          </div>
+        </div>
     </div>
+
+
+      <div className="Encuentranos">
+        <div className="item">
+          <Image
+            src="/silla.svg"
+            alt="Imagen decorativa"
+            width={400}
+            height={300}
+          />
+        </div>
+        <div className="item i2">
+          <h2 className={corinthia.className}>¡Encuéntranos!</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lobortis sed sapien in pretium. Donec tincidunt, quam vestibulum ultricies.</p>
+        </div>
+        <div className="item">
+          <Image
+            src="/pileta.svg"
+            alt="Imagen decorativa"
+            width={400}
+            height={300}
+          />
+        </div>
+      </div>
+
+      <div className="mapa">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3581361.0577490767!2d-64.4921250948583!3d-28.76664233563793!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94329977b18b6df7%3A0x8f9c10933bb46db8!2sSENTIRSE%20BIEN%20SPA!5e0!3m2!1ses-419!2sar!4v1725247558743!5m2!1ses-419!2sar"
+          width="100%"
+          height="450"
+          style={{ border: 0 }}
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
     </>
   );
 }
--- proyecto-spa-web/src/app/services/page.js
+++ proyecto-spa-web/src/app/services/page.js
@@ -1,51 +1,77 @@
 import React from "react";
 import Image from "next/image";
+import styles from  "../../styles/Services.module.css";
+import { Corinthia } from "@next/font/google";
+
+const corinthia = Corinthia({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export default function page() {
   return (
-    <div className="services-container">
-    <h1 className="services-header">Servicios</h1>
-    <div className="service-block">
+    <div className={styles["services-container"]}>
+    <h1 className={styles["services-header"]}>Servicios</h1>
+      <div className={styles["service-block"]}>
+        <div className={styles["services-titles"]}>
+        <div className={styles["minicont"]}>
         <h2>Masajes</h2>
-      <Image
-        src="/medical-person.jpg"
+        
+      <Image className="img-services"
+        src="/fotomasajeser.svg"
         alt="Persona en atuendo médico"
         width={300}
         height={200}
-      />
+            />
+          
+          </div>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lobortis sed sapien in pretium. Donec tincidunt, quam vestibulum ultricies egestas, dolor sem laoreet orci, quis finibus justo quam at nibh. Vestibulum ipsum tortor, suscipit non enim vitae. Tincidunt scelerisque augue. Nunc quis fringilla magna, vel sollicitudin quam.</p>
+          </div>
+          
     </div>
-    <div className="service-block">
+      <div className={styles["service-block"]}>
+        <div className={styles["services-titles-r"]}>
+        <div className={styles["minicont"]}>
         <h2>Belleza</h2>
       <Image
-        src="/dropper-bottle.jpg"
+        src="/belleza.svg"
         alt="Manos con gotero"
         width={300}
         height={200}
-      />
+            />
+            </div>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lobortis sed sapien in pretium. Donec tincidunt, quam vestibulum ultricies egestas, dolor sem laoreet orci, quis finibus justo quam at nibh. Vestibulum ipsum tortor, suscipit non enim vitae. Tincidunt scelerisque augue. Nunc quis fringilla magna, vel sollicitudin quam.</p>
-    </div>
-    <div className="service-block">
+        </div>
+        </div>
+      <div className={styles["service-block"]}>
+        <div className={styles["services-titles"]}>
+        <div className={styles["minicont"]}>
         <h2>Tratamientos Faciales</h2>
       <Image
-        src="/spa-items.jpg"
+        src="/tratamientofacial.svg"
         alt="Artículos de spa"
         width={300}
         height={200}
-      />
+            />
+            </div>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lobortis sed sapien in pretium. Donec tincidunt, quam vestibulum ultricies egestas, dolor sem laoreet orci, quis finibus justo quam at nibh. Vestibulum ipsum tortor, suscipit non enim vitae. Tincidunt scelerisque augue. Nunc quis fringilla magna, vel sollicitudin quam.</p>
-    </div>
-    <div className="service-block">
+        </div>
+        </div>
+      <div className={styles["service-block"]}>
+        <div className={styles["services-titles-r"]}>
+        <div className={styles["minicont"]}>
         <h2>Tratamientos Corporales</h2>
       <Image
-        src="/spa-items.jpg"
+        src="/tratamientocor.svg"
         alt="Artículos de spa"
         width={300}
         height={200}
-      />
+            />
+            </div>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lobortis sed sapien in pretium. Donec tincidunt, quam vestibulum ultricies egestas, dolor sem laoreet orci, quis finibus justo quam at nibh. Vestibulum ipsum tortor, suscipit non enim vitae. Tincidunt scelerisque augue. Nunc quis fringilla magna, vel sollicitudin quam.</p>
     </div>
-    
+    </div>
+        
   </div>
   );
 }
