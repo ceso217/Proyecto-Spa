d8e0199a8f6a2457832744efb188d347b193847a
LeonardoBrabo <leonardo.david.brabo@gmail.com>
2024-09-14 19:51:31 -0300

    componentes creados de las CardNotice, cambio de color fondo /notice y cambio color botones notice

--- proyecto-spa-web/src/app/notice/page.jsx
+++ proyecto-spa-web/src/app/notice/page.jsx
@@ -1,10 +1,34 @@
 import React from "react";
-import Cards from "@/components/cards";
+import CardNotice from "@/components/CardNotice";
 
 const Notice = () => {
   return (
-    <div>
-      <Cards />
+    <div className="min-h-screen bg-customBackground flex justify-center items-center py-20">
+      <div className="container mx-auto p-12 bg-gray-100 rounded-xl">
+        <h1 className="text-4xl uppercase font-bold from-current mb-8">
+          Ultimas Noticias
+        </h1>
+        <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 gap-4 space-y-4 sm:space-y-0">
+          <CardNotice
+            imageSrc="https://i.imgur.com/lmYYa2s.png"
+            title="Papież gigant"
+            location="New York"
+            description="Częstochowski pomnik Jana Pawła II wyjątkowo interesująco wpisuje się w poprzemysłowy krajobraz tego miasta."
+          />
+          <CardNotice
+            imageSrc="https://i.imgur.com/csPYilq.png"
+            title="Papież gigant"
+            location="New York"
+            description="Powstały w 2013 roku, uchodzi za najwyższego Karola Wojtyłę w Polsce."
+          />
+          <CardNotice
+            imageSrc="https://i.imgur.com/chvO4cX.png"
+            title="Papież gigant"
+            location="New York"
+            description="Częstochowski pomnik Jana Pawła II wyjątkowo interesująco wpisuje się w poprzemysłowy krajobraz tego miasta."
+          />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ proyecto-spa-web/src/components/CardNotice.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+export default function CardNotice({ imageSrc, title, location, description }) {
+  return (
+    <div className="bg-white">
+      <div className="shadow-lg hover:shadow-xl transform transition duration-500 hover:scale-105">
+        <div>
+          <img className="w-full" src={imageSrc} alt={title} />
+          <div className="px-4 py-2">
+            <h1 className="text-xl font-gray-700 font-bold">{title}</h1>
+            <div className="flex space-x-2 mt-2">
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-7 w-7 text-yellow-400"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                </svg>
+              </span>
+              <h3 className="text-lg text-gray-600 font-semibold mb-2">
+                {location}
+              </h3>
+            </div>
+            <p className="text-sm tracking-normal">{description}</p>
+            <button className="mt-12 w-full text-center bg-green-services-300 py-2 rounded-lg">
+              Read more
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
