4d7f7068aa1ede41f34cb982422b9d1fdec62923
LeonardoBrabo <leonardo.david.brabo@gmail.com>
2024-09-15 22:33:46 -0300

    Estaba probando meter el carrusel tambien, instalando material Tailwind

--- proyecto-spa-web/tailwind.config.js
+++ proyecto-spa-web/tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+module.exports = withMT({
+  content: ["./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
 
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+    "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
     extend: {
       colors: {
@@ -21,4 +22,4 @@ module.exports = {
     },
   },
   plugins: [],
-};
+});
