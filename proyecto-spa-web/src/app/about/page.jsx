85ed3b290c9e37bcebb43c2c7a6af03654207d95
LeonardoBrabo <leonardo.david.brabo@gmail.com>
2024-09-16 14:22:24 -0300

    fix sección empleo, front y cambio color bg Quienes somos

--- proyecto-spa-web/src/app/about/page.jsx
+++ proyecto-spa-web/src/app/about/page.jsx
@@ -4,23 +4,21 @@ import "../../styles/landing.css";
 import { cormorant, montserrat } from "../ui/fonts";
 import Carousel from "@/components/Carousel";
 
-
-
 export default function page() {
   const message =
     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s...";
   return (
     <>
-    <div className="AboutUs" id="somos">
-      <div className="item">
-        <h2
+      <div className="AboutUs bg-orange-100" id="somos">
+        <div className="item">
+          <h2
             className="text-7xl text-green-services-300 text-center flex-col py-10"
-            style={cormorant.style}>
+            style={cormorant.style}
+          >
             Quienes Somos
           </h2>
           {/* Sección de "The Team" */}
-          <div className="h-[calc(100vh-20rem)] flex items-center justify-content flex-row" >
-
+          <div className="h-[calc(100vh-20rem)] flex items-center justify-content flex-row">
             <div className="row">
               <div className="team-item">
                 <img src="/logo.svg" className="team-img" alt="LEO" />
@@ -36,7 +34,13 @@ export default function page() {
                 <div className="team-info">
                   <p className="position">Dra. General</p>
                 </div>
-                <p className="description">Apasionada especialista en bienestar con más de 10 años de experiencia en la industria del spa y la salud holística. Su enfoque integral y su habilidad para conectar con las personas la convierten en una profesional excepcional en el arte del masaje terapéutico y los tratamientos relajantes.</p>
+                <p className="description">
+                  Apasionada especialista en bienestar con más de 10 años de
+                  experiencia en la industria del spa y la salud holística. Su
+                  enfoque integral y su habilidad para conectar con las personas
+                  la convierten en una profesional excepcional en el arte del
+                  masaje terapéutico y los tratamientos relajantes.
+                </p>
               </div>
               <div className="team-item">
                 <img src="/logo.svg" className="team-img" alt="CECI" />
@@ -50,15 +54,15 @@ export default function page() {
           </div>
         </div>
       </div>
-      <div>
-      <h2
-            className="text-7xl text-green-services-300 text-center flex-col py-10"
-            style={cormorant.style}>
-            Galeria de Fotos
-          </h2>
-      <Carousel/>
+      <div className="bg-orange-100">
+        <h2
+          className="text-7xl text-green-services-300 text-center flex-col py-10"
+          style={cormorant.style}
+        >
+          Galeria de Fotos
+        </h2>
+        <Carousel />
       </div>
-    
     </>
   );
 }
--- proyecto-spa-web/src/app/employment/page.jsx
+++ proyecto-spa-web/src/app/employment/page.jsx
@@ -1,4 +1,4 @@
-
+import { corinthia, cormorant, montserrat } from "@/app/ui/fonts";
 
  const EmploymentPage = () => {
   return (
@@ -11,14 +11,14 @@
         rel="stylesheet"
         href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css"
       />
-      <section className="pb-20 relative block bg-green-services-200">
+      <section className="pb-20 relative block bg-orange-100">
         <div className="container mx-auto px-4 lg:pt-24 lg:pb-64">
           <div className="flex flex-wrap text-center justify-center">
             <div className="w-full lg:w-6/12 px-4">
-              <h2 className="text-4xl font-semibold text-white">
+              <h2 className="text-7xl font-bold from-current mb-8 text-green-services-300 text-center" style={cormorant.style}>
               Únete a Nuestro Equipo 
               </h2>
-              <p className="text-lg leading-relaxed mt-4 mb-4 text-blueGray-400">
+              <p className="text-lg leading-relaxed mt-4 mb-4 text-blueGray-500">
               Descubre tu potencial en un entorno dedicado al bienestar y al crecimiento personal.
               </p>
             </div>
@@ -28,10 +28,10 @@
               <div className="text-lightBlue-300 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
                 <i className="fas fa-medal text-xl" />
               </div>
-              <h6 className="text-xl mt-5 font-semibold text-white">
+              <h6 className="text-xl mt-5 font-semibold text-green-services-300">
               Atención Excepcional
               </h6>
-              <p className="mt-2 mb-4 text-blueGray-400">
+              <p className="mt-2 mb-4 text-blueGray-500">
               Sumérgete en nuestra filosofía de nutrir el talento y proporcionar servicios sobresalientes que aseguren que nuestros clientes se sientan rejuvenecidos.
               </p>
             </div>
@@ -39,10 +39,10 @@
               <div className="text-blueGray-800 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
                 <i className="fas fa-poll text-xl" />
               </div>
-              <h5 className="text-xl mt-5 font-semibold text-white">
+              <h5 className="text-xl mt-5 font-semibold text-green-services-300">
               Expande Tus Habilidades
               </h5>
-              <p className="mt-2 mb-4 text-blueGray-400">
+              <p className="mt-2 mb-4 text-blueGray-500">
               Aprovecha las oportunidades de desarrollo profesional mientras contribuyes a nuestra misión de bienestar holístico.
               </p>
             </div>
@@ -50,31 +50,31 @@
               <div className="text-blueGray-800 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
                 <i className="fas fa-lightbulb text-xl" />
               </div>
-              <h5 className="text-xl mt-5 font-semibold text-white">
+              <h5 className="text-xl mt-5 font-semibold text-green-services-300">
               Comienza Tu Viaje
               </h5>
-              <p className="mt-2 mb-4 text-blueGray-400">
+              <p className="mt-2 mb-4 text-blueGray-500">
               Inicia una carrera donde cada día se trata de promover la salud, el equilibrio y la relajación para quienes buscan tranquilidad.
               </p>
             </div>
           </div>
         </div>
       </section>
-      <section className="relative block pt-24 lg:pt-0 bg-green-services-200">
+      <section className="relative block pt-24 lg:pt-0 bg-orange-100">
         <div className="container mx-auto px-4">
           <div className="flex flex-wrap justify-center lg:-mt-64 -mt-48">
             <div className="w-full lg:w-6/12 px-4">
-              <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200">
+              <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-green-services-100">
                 <div className="flex-auto p-5 lg:p-10">
-                  <h4 className="text-2xl font-semibold text-center">
+                  <h4 className="text-3xl font-semibold text-center text-crema" style={cormorant.style}>
                     ¿Te gustaría trabajar con nostros?
                   </h4>
-                  <p className="leading-relaxed mt-1 mb-4 text-blueGray-500 text-center">
+                  <p className="leading-relaxed mt-1 mb-4 text-crema text-center pt-4">
                     Completa este formulario y dinos por qué te gustaría unirte a nuestro equipo. ¡Esperamos con interés conocerte!
                   </p>
                   <div className="relative w-full mb-3 mt-8">
                     <label
-                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                      className="block uppercase text-crema text-xs font-bold mb-2"
                       htmlFor="full-name"
                     >
                       Nombre completo
@@ -87,7 +87,7 @@
                   </div>
                   <div className="relative w-full mb-3">
                     <label
-                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                      className="block uppercase text-crema text-xs font-bold mb-2"
                       htmlFor="email"
                     >
                       Email
@@ -100,7 +100,7 @@
                   </div>
                   <div className="relative w-full mb-3">
                     <label
-                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                      className="block uppercase text-crema text-xs font-bold mb-2"
                       htmlFor="message"
                     >
                       Mensaje
@@ -115,7 +115,7 @@
                   </div>
                   <div className="text-center mt-6">
                     <button
-                      className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                      className="bg-green-services-300 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                       type="button"
                     >
                       Enviar Mensaje
