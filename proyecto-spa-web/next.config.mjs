76704a4a28f77086fea11a8c65f2015106400289
ceso217 <baronicecilio@gmail.com>
2024-11-17 21:02:17 -0300

    prueba

--- proyecto-spa-web/next.config.mjs
+++ proyecto-spa-web/next.config.mjs
@@ -1,6 +1,24 @@
-const withPWA = require("next-pwa")({
-  disable: process.env.NODE_ENV === "development",
-});
-module.exports = withPWA({
-  // Configuración adicional
-});
+import withPWA from "next-pwa";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "unsplash.com",
+      "images.unsplash.com",
+      "media.istockphoto.com",
+      "plus.unsplash.com",
+      "www.f1latam.com",
+      "shawellness.com",
+      "img2.rtve.es",
+    ],
+  },
+};
+
+// export default withPWA({
+//   dest: "public", // Configuración específica para PWA
+// })(nextConfig); // Aplica únicamente sobre la configuración base
+
+export default withPWA({
+  disable: process.env.NODE_ENV === "development", // Configuración específica para PWA
+})(nextConfig); // Aplica únicamente sobre la configuración base
